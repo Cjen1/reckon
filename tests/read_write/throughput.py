dae70f5dde4de2976a6019fb01bd37c7ad903cbe
cjj39 <cjj39@caelum-507.cl.cam.ac.uk>
2018-07-04 16:29:07 +0100

    Refactor throughput.py, start etcd.setup

--- tests/read_write/throughput.py
+++ tests/read_write/throughput.py
@@ -1,6 +1,6 @@
 import time
 import numpy as np
-import bench_etcd as b_etcd
+import etcd/bench_etcd as b_etcd
 import sys
 
 class Concsys: pass
@@ -23,31 +23,36 @@ def gen_ops(ratio, number, readgen, writegen):
             readgen() if i < ratio else writegen() 
             for i in np.random.uniform(0, 1, number)] 
 
-#-------- test throughput benchmark ----------
-#
-#testsys = Concsys()
-#testsys.readgen  = lambda : lambda : time.sleep(0.01)
-#testsys.writegen = lambda : lambda : time.sleep(0.01)
-#
-#throughputs = [bench_throughput(testsys) for i in range(2)]
-#
-#print throughputs
-#
-#-------- etcd throughput benchmark ----------
-
-print "-------- etcd throughput benchmark ----------"
-
-hosts = ["caelum-504.cl.cam.ac.uk", "caelum-505.cl.cam.ac.uk", "caelum-506.cl.cam.ac.uk"]
-
-maxKeyValue = 100
-
-client = b_etcd.setup(hosts, maxKeyValue)
-
-etcdsys = Concsys()
-etcdsys.readgen  =  lambda: b_etcd.readgen(maxKeyValue, client)
-etcdsys.writegen =  lambda: b_etcd.writegen(maxKeyValue, client)
+def test_throughputs(number):
+    print "-------- test throughput benchmark ----------"
+    
+    testsys = Concsys()
+    testsys.readgen  = lambda : lambda : time.sleep(0.01)
+    testsys.writegen = lambda : lambda : time.sleep(0.01)
+    
+    throughputs = [bench_throughput(testsys) for i in range(2)]
+    
+    return throughputs
 
 
-throughputs = [bench_throughput(etcdsys) for i in range(100)]
+def etcd_throughputs(number):
+    print "-------- etcd throughput benchmark ----------"
 
-print throughputs
+    hosts = ["caelum-504.cl.cam.ac.uk", "caelum-505.cl.cam.ac.uk", "caelum-506.cl.cam.ac.uk"]
+
+    maxKeyValue = 100
+
+    client = b_etcd.setup(hosts, maxKeyValue)
+
+    etcdsys = Concsys()
+    etcdsys.readgen  =  lambda: b_etcd.readgen(maxKeyValue, client)
+    etcdsys.writegen =  lambda: b_etcd.writegen(maxKeyValue, client)
+
+
+    throughputs = [bench_throughput(etcdsys) for i in range(number)]
+
+    return throughputs
+
+
+if __name__ == "__main__":
+    print etcd_throughputs(100)

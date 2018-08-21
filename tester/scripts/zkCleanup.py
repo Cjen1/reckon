9180eb38482c42fd183e66b9b49a9ac515000ed0
dks28 <dks28@cam.ac.uk>
2018-08-21 13:47:28 +0100

    fixed zk scripts

--- tester/scripts/zkCleanup.py
+++ tester/scripts/zkCleanup.py
@@ -1,6 +1,6 @@
 from subprocess import call
 def rmZK(host):
-	call(["ssh", host, "docker rm -f consul"])
+	call(["ssh", host, "docker rm -f zookeeper"])
 
 hosts = ["caelum-50{k}.cl.cam.ac.uk".format(k=str(i+4)) for i in range(5)]
 
--- tester/scripts/zookeeper_setup.py
+++ tester/scripts/zookeeper_setup.py
@@ -3,4 +3,4 @@ from sys import argv
 
 hosts = argv[1].split(",")
 
-call(["python", "scripts/zookeeper" + len(hosts) + "_setup.py", argv[1]])
+call(["python", "scripts/zookeeper" + str(len(hosts)) + "_setup.py", argv[1]])

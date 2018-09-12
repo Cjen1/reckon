6213d3648a03254cb28b619e589a225cc1d87b03
Daniel Saaw <dks28@caelum-207.cl.cam.ac.uk>
2018-09-12 17:40:44 +0100

    Implement leader killing and arbitray host killing

--- tester/scripts/consul_stop.py
+++ tester/scripts/consul_stop.py
@@ -12,7 +12,10 @@ def killLeader(cluster):
     host_lookup = {socket.gethostbyname(host) : host for host in cluster}
 
     resp = requests.get('http://127.0.0.1:8500/v1/status/leader')
-    leader_ip = resp.split(':')[0]
+    # Remove surrounding quotes then select first part of "127.0.0.1:8500"
+    leader_ip = resp.text[1:-1].split(':')[0]
+    print(leader_ip)
+    print(host_lookup)
 
     leader = host_lookup[leader_ip]
 
@@ -24,7 +27,7 @@ def killLeader(cluster):
 
 
 parser = argparse.ArgumentParser(description='Stops the either a given server or the leader of a cluster')
-parser.add_argument('--hosts', '-h')
+parser.add_argument('--hosts')
 parser.add_argument('--leader', '-l', action='store_true')
 args = parser.parse_args()
 

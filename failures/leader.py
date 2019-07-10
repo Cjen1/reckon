f0a1b7954a3cb2eb1978934182fedd4f69dec6a4
Cjen1 <cjj39@cam.ac.uk>
2019-07-10 15:48:29 +0100

    Added leader failure

--- failures/leader.py
+++ failures/leader.py
@@ -5,6 +5,21 @@ from mininet.link import TCLink
 from mininet.log import info, setLogLevel
 setLogLevel('info')
 
+def parse_resp(resp):
+    assert False
+    return None
+
 def failure_setup(net):
-    return (lambda:print("fail 1"),
-            lambda:print("fail 2"))
+hosts = [ net[hostname] for hostname in filter(lambda i: i[0] == 'd', net.keys())
+        ]
+ips = [host.IP() for host in hosts]
+
+    cmd = ""
+    leader_ip = parse_resp(hosts[0].cmd(cmd))
+    leader = hosts[ips.index(leader_ip)]
+    leader_name = leader.name
+
+    return [
+            lambda: net.configLinkStatus('s1', leader_name, 'down'),
+            lambda: net.configLinkStatus('s1', leader_name, 'up')
+            ]

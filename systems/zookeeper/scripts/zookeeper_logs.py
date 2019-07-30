e27dc1e492f6085ec10233a5d0623997c09d6d68
dks28 <dks28@cam.ac.uk>
2019-07-30 14:51:01 +0100

    Fixed up Zookeeper

--- systems/zookeeper/scripts/zookeeper_logs.py
+++
@@ -1,6 +0,0 @@
-from subprocess import call
-from sys import argv
-
-test_name = argv[1]
-
-call(["ssh", "caelum-508", "docker logs zookeeper", "&>", "logs/" + test_name + "_zookeeper.logs"])
--- systems/zookeeper/scripts/zookeeper_setup.py
+++
@@ -1,6 +0,0 @@
-from subprocess import call
-from sys import argv
-
-hosts = argv[1].split(",")
-
-call(["python", "scripts/zookeeper" + str(len(hosts)) + "_setup.py", argv[1]])

17db9e472929547193bc2adad2c4f101e8d5e19b
Qi Li <qili@yelp.com>
2020-11-25 01:12:25 +0000

    Create UniformOpsProvider to generate operations for client

--- scripts/test_entrypoint.sh
+++ scripts/test_entrypoint.sh
@@ -4,7 +4,7 @@ service openvswitch-switch start
 ovs-vsctl set-manager ptcp:6640
 
 python benchmark.py etcd_go \
-  simple --topo_args n=1,nc=1 uniform --dist_args write_ratio=1 none \
+  simple --topo_args n=1,nc=1 uniform --write_ratio 1 none \
   --benchmark_config rate=1000,duration=10,dest=./res,logs=./logs `realpath .`
 
 service openvswitch-switch stop

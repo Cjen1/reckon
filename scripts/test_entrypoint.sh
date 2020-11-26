d3f64faf5f05d56e384e840138c5dda5ffbe4877
Cjen1 <cjj39@cam.ac.uk>
2020-11-26 10:51:29 +0000

    Check failing test actually fails CI

--- scripts/test_entrypoint.sh
+++ scripts/test_entrypoint.sh
@@ -4,7 +4,11 @@ service openvswitch-switch start
 ovs-vsctl set-manager ptcp:6640
 
 python benchmark.py etcd_go \
-  simple --topo_args n=1,nc=1 uniform --write-ratio 1 none \
+  simple --topo_args n=1,nc=1 uniform --writ-ratio 1 none \
   --benchmark_config rate=1000,duration=10,dest=./res,logs=./logs `realpath .`
 
+EXITCODE=$?
+
 service openvswitch-switch stop
+
+exit $?
